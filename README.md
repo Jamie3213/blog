5493311a5a11ffa04d72092f1f30b70bdd461f50
Jamie Hargreaves <41867515+Jamie3213@users.noreply.github.com>
2022-01-08 00:27:01 +0000

    Update terraform.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Website                    |  Infrastructure
 :------------------------- |:-------------------------
-![](https://codebuild.eu-west-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoicG9YczVZY0d1Wkd1RzlESnMzeWc0QjJ4emRBQmZpSFkzUm5yU21wYm5QT2l4SUIwREdlOTZuTkhjZkEwRHcwaTRaVEhJb2JlYVIxaFJVL3ltc09xTmh3PSIsIml2UGFyYW1ldGVyU3BlYyI6IlhnVVBveEtLWi9XZVBNanciLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)  |  ![](https://codebuild.eu-west-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoicG9YczVZY0d1Wkd1RzlESnMzeWc0QjJ4emRBQmZpSFkzUm5yU21wYm5QT2l4SUIwREdlOTZuTkhjZkEwRHcwaTRaVEhJb2JlYVIxaFJVL3ltc09xTmh3PSIsIml2UGFyYW1ldGVyU3BlYyI6IlhnVVBveEtLWi9XZVBNanciLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)
+![](https://codebuild.eu-west-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoidWp6bytkcnRZWWdrTm4xR1FnR2hBRmZxTW93YzdmYUV5OHZObXNTSjRFUVRpMU9wRUM4Wk5qbkxSUzB2K2o5WUdLL3hQb3Nld1FEUmNkRjh1T3NHUUcwPSIsIml2UGFyYW1ldGVyU3BlYyI6IldualhzeXNyekZLaW1CL20iLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)  |  ![](https://codebuild.eu-west-1.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoidWp6bytkcnRZWWdrTm4xR1FnR2hBRmZxTW93YzdmYUV5OHZObXNTSjRFUVRpMU9wRUM4Wk5qbkxSUzB2K2o5WUdLL3hQb3Nld1FEUmNkRjh1T3NHUUcwPSIsIml2UGFyYW1ldGVyU3BlYyI6IldualhzeXNyekZLaW1CL20iLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)
 
 # AWS Static Blog
 
--- site/terraform/post_deploy/main.tf
+++ site/terraform/post_deploy/main.tf
@@ -1,56 +1,58 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 /* ----------------------------- Input variables ---------------------------- */
 
 variable "project" {
-    type        = string
-    description = "An abbreviation for the project the resources relate to."
+  type        = string
+  description = "An abbreviation for the project the resources relate to."
 
-    validation {
-        condition     = can(regex("[a-z]*", var.project))
-        error_message = "Project abbreviation must be lower case letters only."
-    }
+  validation {
+    condition     = can(regex("[a-z]*", var.project))
+    error_message = "Project abbreviation must be lower case letters only."
+  }
 }
 
 variable "created_by" {
-    type        = string
-    description = "The name of the user who created the resource."
+  type        = string
+  description = "The name of the user who created the resource."
 }
 
 variable "artifact_bucket_name" {
-    type        = string
-    description = "The name of the S3 bucket where build artifacts should be stored."
+  type        = string
+  description = "The name of the S3 bucket where build artifacts should be stored."
 }
 
 variable "blog_bucket_name" {
-    type        = string
-    description = "The name of the S3 bucket where the blog is hosted."
+  type        = string
+  description = "The name of the S3 bucket where the blog is hosted."
 }
 
 variable "log_group_name" {
-    type        = string
-    description = "The name of the CloudWatch Log Group that CodeBuild Log Streams should write to."
+  type        = string
+  description = "The name of the CloudWatch Log Group that CodeBuild Log Streams should write to."
 }
 
 /* -------------------------------- Providers ------------------------------- */
 
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 
-    default_tags { 
-        tags = {
-            Project     = var.project,
-            CreatedBy   = var.created_by
-        }
+  default_tags { 
+    tags = {
+      Project     = var.project,
+      CreatedBy   = var.created_by
     }
+  }
 }
+
+/* ------------------------------ Data sources ------------------------------ */
 
 data "aws_region" "current" {}
 
@@ -65,7 +67,7 @@ data "aws_s3_bucket" "blog_bucket" {
 }
 
 data "aws_cloudwatch_log_group" "log_group" {
-    name = var.log_group_name
+  name = var.log_group_name
 }
 
 /* -------------------------------- Resources ------------------------------- */
@@ -124,64 +126,105 @@ POLICY
 }
 
 resource "aws_codebuild_project" "build" {
-    name           = "build-jamie-blog-infra"
-    description    = "Builds Hugo blog static files."
-    source_version = "main"
-    service_role   = aws_iam_role.iam_role.arn
-    build_timeout  = 5
-    badge_enabled  = true
+  name           = "build-jamie-blog-site"
+  description    = "Builds Hugo blog static files."
+  source_version = "main"
+  service_role   = aws_iam_role.iam_role.arn
+  build_timeout  = 5
+  badge_enabled  = true
 
-    source {
-        type                = "GITHUB"
-        location            = "https://github.com/Jamie3213/blog.git"
-        buildspec           = "site/release/buildspec_build.yml"
-        report_build_status = true
+  source {
+    type                = "GITHUB"
+    location            = "https://github.com/Jamie3213/blog.git"
+    buildspec           = "site/release/buildspec_build.yml"
+    report_build_status = true
 
-        git_submodules_config {
-            fetch_submodules = true
-        }
+    git_submodules_config {
+      fetch_submodules = true
     }
+  }
 
-    environment {
-        compute_type = "BUILD_GENERAL1_SMALL"
-        image        = "aws/codebuild/standard:5.0"
-        type         = "LINUX_CONTAINER"
-    }
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:5.0"
+    type         = "LINUX_CONTAINER"
+  }
 
-    artifacts {
-        type      = "S3"
-        location  = data.aws_s3_bucket.artifact_bucket.id
-        name      = "build.zip"
-        path      = "blog/infra/"
-        packaging = "ZIP"
-    }
+  artifacts {
+    type      = "S3"
+    location  = data.aws_s3_bucket.artifact_bucket.id
+    name      = "build.zip"
+    path      = "blog/site/"
+    packaging = "ZIP"
+  }
 
-    logs_config {
-        cloudwatch_logs {
-            group_name  = data.aws_cloudwatch_log_group.log_group.name
-            stream_name = "infra/codebuild/build"
-        }
+  logs_config {
+    cloudwatch_logs {
+      group_name  = data.aws_cloudwatch_log_group.log_group.name
+      stream_name = "site/codebuild/build"
     }
+  }
 }
 
 resource "aws_codebuild_webhook" "webhook" {
   project_name = aws_codebuild_project.build.name
   build_type   = "BUILD"
 
-    filter_group {
-        filter {
-            type    = "EVENT"
-            pattern = "PUSH"
-        }
-
-        filter {
-            type    = "HEAD_REF"
-            pattern = "main"
-        }
-
-        filter {
-            type    = "FILE_PATH"
-            pattern = "site/*"
-        }
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
     }
+
+    filter {
+      type    = "HEAD_REF"
+      pattern = "main"
+    }
+
+    filter {
+      type    = "FILE_PATH"
+      pattern = "site/*"
+    }
+  }
+}
+
+resource "aws_codebuild_project" "deploy" {
+  name           = "deploy-jamie-blog-site"
+  description    = "Deploy the static Hugo blog to Amazon S3."
+  source_version = "main"
+  service_role   = aws_iam_role.iam_role.arn
+  build_timeout  = 5
+
+  source {
+    type                = "GITHUB"
+    location            = "https://github.com/Jamie3213/blog.git"
+    buildspec           = "site/release/buildspec_deploy.yml"
+    report_build_status = true
+
+    git_submodules_config {
+      fetch_submodules = true
+    }
+  }
+
+  environment {
+    compute_type = "BUILD_GENERAL1_SMALL"
+    image        = "aws/codebuild/standard:5.0"
+    type         = "LINUX_CONTAINER"
+  }
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = data.aws_cloudwatch_log_group.log_group.name
+      stream_name = "site/codebuild/deploy"
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "deploy_trigger" {
+  name        = "trigger-jamie-blog-deploy"
+  description = "Triggers the Blog deploy CodeBuild project when the build artifacts are updated."
 }
