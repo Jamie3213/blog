eae600731673467ab3ecfc2966bb47336f1afc60
Jamie Hargreaves <41867515+Jamie3213@users.noreply.github.com>
2022-01-09 00:41:18 +0000

    Update buildspecs.

--- infra/lambda/app.py
+++ infra/lambda/app.py
@@ -31,50 +31,51 @@ logger.addHandler(consoleHandler)
 # ---------------------------------- Handler --------------------------------- #
 
 class S3EventNotification(TypedDict):
-    Records: List[Dict[str, Any]]
+  Records: List[Dict[str, Any]]
 
 class LambdaResponse(TypedDict):
-    isBase64Encoded: bool
-    statusCode: int
-    body: str
+  isBase64Encoded: bool
+  statusCode: int
+  body: str
 
 
 def lambda_handler(event: S3EventNotification, context: LambdaContext) -> LambdaResponse:
-    S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-    S3_OBJECT_KEY = os.environ['S3_OBJECT_KEY']
+  S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+  S3_OBJECT_KEY = os.environ['S3_OBJECT_KEY']
 
-    logger.info("Reading config file from Amazon S3.")
-    s3 = boto3.resource("s3")
-    object = s3.Object(S3_BUCKET_NAME, S3_OBJECT_KEY)
+  logger.info("Reading config file from Amazon S3.")
+  s3 = boto3.resource("s3")
+  object = s3.Object(S3_BUCKET_NAME, S3_OBJECT_KEY)
 
-    try:
-        config_binary = object.get() \
-            .get('Body') \
-            .read()
-    except ClientError:
-        logger.error(traceback.format_exc())
-        body = json.dumps(f"Failed to read config file '{S3_OBJECT_KEY}' from bucket '{S3_BUCKET_NAME}'.")
-        return LambdaResponse(isBase64Encoded=False, statusCode=500, body=body)
+  try:
+    config_binary = object.get() \
+      .get('Body') \
+      .read()
+  except ClientError:
+    logger.error(traceback.format_exc())
+    body = json.dumps(f"Failed to read config file '{S3_OBJECT_KEY}' from bucket '{S3_BUCKET_NAME}'.")
+    return LambdaResponse(isBase64Encoded=False, statusCode=500, body=body)
 
-    config = yaml.safe_load(config_binary)
+  config = yaml.safe_load(config_binary)
 
-    change_file = event['Records'][0] \
-        ['s3'] \
-        ['object'] \
-        ['key']
+  change_file = event['Records'][0] \
+    ['s3'] \
+    ['object'] \
+    ['key']
 
-    base_folder = str.split(change_file, "/")[1]
-    codebuild_project = config['Folder'][base_folder]
+  base_folder = str.split(change_file, "/")[1]
+  codebuild_project = config['Folder'][base_folder]
 
-    logger.info(f"Starting CodeBuild project '{codebuild_project}'")
-    codebuild = boto3.client("codebuild")
-    build = codebuild.start_build(projectName=codebuild_project)
-    build_status = build['build']['buildStatus']
+  logger.info(f"Starting CodeBuild project '{codebuild_project}'")
+  codebuild = boto3.client("codebuild")
+  build = codebuild.start_build(projectName=codebuild_project)
+  build_status = build['build']['buildStatus']
 
-    if build_status not in ['SUCCEEDED', 'IN_PROGRESS']:
-        msg = f"Failed to CodeBuild project '{codebuild_project}', build status returned '{build_status}'"
-        logger.error(msg)
-        return LambdaResponse(isBase64Encoded=False, statusCode=500, body=json.dumps(msg))
-    else:
-        body = json.dumps(f"Build started and returned status '{build_status}'")
-        return LambdaResponse(isBase64Encoded=False, statusCode=200, body=body)
+  if build_status not in ['SUCCEEDED', 'IN_PROGRESS']:
+    msg = f"Failed to CodeBuild project '{codebuild_project}', build status returned '{build_status}'"
+    logger.error(msg)
+    return LambdaResponse(isBase64Encoded=False, statusCode=500, body=json.dumps(msg))
+  else:
+    msg = f"Build started and returned status '{build_status}'"
+    logger.info(msg)
+    return LambdaResponse(isBase64Encoded=False, statusCode=200, body=json.dumps(msg))
